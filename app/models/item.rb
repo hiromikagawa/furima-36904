463c78f0397c25677ea14d68d88c9e7bd3249257
hiromikagawa <23.01.13hh@ezweb.ne.jp>
2021-10-09 22:15:30 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to:postage
   belongs_to:region
   belongs_to:status
+  has_one_attached :image
 
   validates :name, :text, :prise, presence:true
   validates :category, :status, :postage, :region, :day, numericality: { other_than: 1 } 
