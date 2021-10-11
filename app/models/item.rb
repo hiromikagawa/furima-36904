8a519b03e761f7f42c140b43a42cf12379022739
hiromikagawa <23.01.13hh@ezweb.ne.jp>
2021-10-11 17:12:55 +0900

    テストコード実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
   belongs_to:user
   has_one_attached :image
 
-  validates :name, :text, :prise, presence:true
+  validates :name, presence:true
+  validates :text, presence:true
+  validates :image, presence:true
 
   validates :category_id, presence:true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, presence:true, numericality: { other_than: 1 , message: "can't be blank"}
@@ -11,8 +13,11 @@ class Item < ApplicationRecord
   validates :region_id, presence:true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :day_id, presence:true, numericality: { other_than: 1 , message: "can't be blank"} 
 
-  validates :prise, format: { with: /\A[0-9]+\z/, message: "is invalid Input half-width characters" }
-  validates :prise, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: " is out of setting range" } 
+  # with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+  validates :prise, presence: true
+  validates :prise, numericality: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
+  validates :prise, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: " is out of setting range" }
+  #end 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :day, :postage, :region, :status
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.sentence}
+    text {Faker::Lorem.sentence}
+    category_id{2}
+    status_id{2}
+    postage_id{2}
+    region_id{2}
+    day_id{2}
+    prise {2000}
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
