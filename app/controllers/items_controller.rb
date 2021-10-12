80e6183d8e814c0212d4f0f65152e97ab071825b
hiromikagawa <23.01.13hh@ezweb.ne.jp>
2021-10-12 15:55:48 +0900

    Rubocopの実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :text, :category_id, :status_id, :postage_id, :region_id, :day_id,:prise) .merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :text, :category_id, :status_id, :postage_id, :region_id, :day_id,
+                                 :prise).merge(user_id: current_user.id)
   end
 end
