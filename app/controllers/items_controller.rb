36f46e7694e36cce9302effbd92880daa1e67e9c
hiromikagawa <23.01.13hh@ezweb.ne.jp>
2021-10-12 20:20:04 +0900

    rubocopの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -20,9 +20,8 @@ class ItemsController < ApplicationController
   end
 
   def show
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
-
 
   private
 
