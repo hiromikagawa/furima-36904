05a666f62a25ebe236421fa48e64f13d63c11847
hiromikagawa <23.01.13hh@ezweb.ne.jp>
2021-10-12 12:06:29 +0900

    商品出品機能修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,14 +12,17 @@ class Item < ApplicationRecord
   validates :region_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
 
-  # with_options presence: true, format: { with: /\A[0-9]+\z/ } do
   validates :prise, presence: true
   validates :prise, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
   validates :prise,
             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                             message: ' is out of setting range' }
-  # end
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :day, :postage, :region, :status
+  belongs_to :category
+  belongs_to :day
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :status
 end
